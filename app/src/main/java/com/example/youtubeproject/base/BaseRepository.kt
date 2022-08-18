6c472599a2bc3a4d4421c09dde58b687eaf245ae
ilgiz05 <zaydamustafaeva@gmail.com>
2022-08-18 13:03:10 +0600

    добавил BaseFragment BaseViewModel BaseRepository

---
+++ app/src/main/java/com/example/youtubeproject/base/BaseRepository.kt
@@ -0,0 +1,10 @@
+package com.example.youtubeproject.base
+
+import com.example.youtubeproject.Either
+import kotlinx.coroutines.Dispatchers
+import java.io.IOException
+
+open class BaseRepository {
+
+
+}
---
+++ app/src/main/java/com/example/youtubeproject/base/BaseViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.youtubeproject.base
+
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel: ViewModel()
